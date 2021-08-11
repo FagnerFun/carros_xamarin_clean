7c7668f3f1789fae9d50d61c3e3bc10192de9459
Fagner Muniz <fagner.santos@dextra-sw.com>
2021-08-11 15:22:17 -0300

    UITest

---
+++ carros_xamarin_clean/carros_xamarin_clean.UITest/AppInitializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace carros_xamarin_clean.UITest
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp
+                    .Android
+                    .InstalledApp("com.fagnerfun.carrosclean")
+                    .EnableLocalScreenshots()
+                    .StartApp();
+            }
+
+            return ConfigureApp.iOS
+                .EnableLocalScreenshots()
+                .StartApp();
+        }
+    }
+}
