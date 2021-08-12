c95fe811d7885240e7b00afdb612b54239af73eb
Fagner Muniz <fagner.santos@dextra-sw.com>
2021-08-12 16:40:37 -0300

    UI test AppCenter

---
+++ carros_xamarin_clean/carros_xamarin_clean.InterfaceTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace carros_xamarin_clean.InterfaceTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
--- carros_xamarin_clean/carros_xamarin_clean.UITest/LoginUI.cs
+++ carros_xamarin_clean/carros_xamarin_clean.UITest/LoginUI.cs
@@ -1,25 +1,20 @@
-﻿using carros_xamarin_clean.Core.Resources;
-using NUnit.Framework;
-//using NUnit.Framework;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
-using TechTalk.SpecFlow;
+using carros_xamarin_clean.Core.Resources;
+using NUnit.Framework;
 using Xamarin.UITest;
 using Xamarin.UITest.Queries;
 
-
-namespace carros_xamarin_clean.UITest.Features.Login
+namespace carros_xamarin_clean.UITest
 {
-    //[Binding]
-    //[Scope(Feature = "LoginRequirements")]
     [TestFixture(Platform.Android)]
     [TestFixture(Platform.iOS)]
-    public class LoginRequirementsSteps
+    public class LoginUI
     {
         IApp app;
-        readonly Platform platform;
+        Platform platform;
 
         static readonly Func<AppQuery, AppQuery> InitialMessage = c => c.Marked("LoginButton").Text(Resource.login);
         static readonly Func<AppQuery, AppQuery> UserEntry = c => c.Marked("UserEntry");
@@ -27,7 +22,7 @@ namespace carros_xamarin_clean.UITest.Features.Login
         static readonly Func<AppQuery, AppQuery> LoginButton = c => c.Marked("LoginButton");
         static readonly Func<AppQuery, AppQuery> CarSreen = c => c.Marked("Carros");
 
-        public LoginRequirementsSteps(Platform platform)
+        public LoginUI(Platform platform)
         {
             this.platform = platform;
         }
@@ -38,7 +33,7 @@ namespace carros_xamarin_clean.UITest.Features.Login
             app = AppInitializer.StartApp(platform);
         }
 
-        //[Given(@"an user at login screen")]
+        //[Test]
         //public void LoginScreenIsDisplayed()
         //{
         //    List<AppResult> results = new List<AppResult>();
@@ -46,35 +41,9 @@ namespace carros_xamarin_clean.UITest.Features.Login
         //    results.AddRange(app.Query(InitialMessage));
         //    app.Screenshot("Login screen.");
 
-        //    //Assert.IsTrue(results.Any());
+        //    Assert.IsTrue(results.Any());
         //}
 
-        //[When(@"he populate user field")]
-        //public void PopulateUserField()
-        //{
-        //    app.EnterText(UserEntry, "user");
-        //}
-
-        //[When(@"he populate password field")]
-        //public void PopulatePasswordField()
-        //{
-        //    app.EnterText(PasswordEntry, "123");
-        //}
-
-        //[When(@"he press login button")]
-        //public void PressLoginButton()
-        //{
-        //    app.Tap(LoginButton);
-        //}
-
-        //[Then(@"he is allowed to access app")]
-        //public void CarScreenIsDisplayed()
-        //{
-        //    List<AppResult> results = new List<AppResult>();
-        //    results.AddRange(app.WaitForElement(CarSreen, "Não foi possivel navegar para carros"));
-        //    app.Screenshot("Cars.");
-        //    //Assert.IsTrue(results.Any());
-        //}
 
         [Test]
         public void LoginSuccessfully()
